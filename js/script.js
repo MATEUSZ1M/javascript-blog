765493866c2aa73c72d63989e19ab732b9d4abdf
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-02-17 13:34:32 +0000

    find the title element

--- js/script.js
+++ js/script.js
@@ -1,77 +1,72 @@
 'use scrict';
-{//dlaczego niektóre bloki kodu są dodatkowo w nawiasach ?
-    const titleClickHandler = function (event) {
-        event.preventDefault();
-        console.log('Link was clicked!')
-        console.log(event)
+const titleClickHandler = function (event) {
+    event.preventDefault();
+    console.log('Link was clicked!')
+    console.log(event)
 
-        /*[DONE] remove class 'active' from all article links */
-        const activeLinks = document.querySelectorAll('.titles a.active');
+    /*[DONE] remove class 'active' from all article links */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-        for (let activeLink of activeLinks) {
-            activeLink.classList.remove('active');
-        }
-        /* [DONE] add class 'active' to the clicked link */
-        const clickedElement = this;
-
-        clickedElement.classList.add('active');
-
-        console.log('clickedElement (with plus): ' + clickedElement);
-        /* [DONE] remove class "active" from all articles */
-        const activeArticles = document.querySelectorAll('.posts article.active'); // dlaczego to co jest w nawiasie
-
-        for (let activeArticle of activeArticles) {
-            activeArticle.classList.remove('active');
-        }
-        /* [DONE] get "href" attribute from the clicked link */
-        const articleSelector = clickedElement.getAttribute('href');
-
-        console.log(articleSelector);
-        /* [DONE] find the correct article using the selector (value of "href" attribute) */
-        const targetArticle = document.querySelector(articleSelector);
-
-        console.log(targetArticle);
-        /* [DONE]add class "active to the correct article" */
-
-        targetArticle.classList.add('active');
-
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
     }
+    /* [DONE] add class 'active' to the clicked link */
+    const clickedElement = this;
+    clickedElement.classList.add('active');
 
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {// jak działają te pętle ???
-        link.addEventListener('click', titleClickHandler);
+    console.log('clickedElement (with plus): ' + clickedElement);
+    /* [DONE] remove class "active" from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
     }
+    /* [DONE] get "href" attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
-    const optArticleSelector = '.post',
-        optTitleSelector = '.post-title',
-        optTitleListSelector = '.titles';
+    console.log(articleSelector);
+    /* [DONE] find the correct article using the selector (value of "href" attribute) */
+    const targetArticle = document.querySelector(articleSelector);
 
-    function generateTitleLinks() {//dlaczego niektóre funkcje nie mają nic w nawiasach 
+    console.log(targetArticle);
+    /* [DONE]add class "active to the correct article" */
 
-        /*[DONE]remove contents of titleList */
-        const titleList = document.querySelector(optTitleListSelector);
-        titleList.innerHTML = '';
-        console.log(titleList)// czy console.log musi być w tej samej funkcji żeby zadziałał jakie warunki muszą być spełnionie żeby działał
-        /*  [DONE] for each article */
-        const articles = optArticleSelector;
+    targetArticle.classList.add('active');
+}
 
-        for(let article of articles ){
-            console.log(articles)
-        }
-        
-        /*[IN PROGRESS] get the article id */
-       
+const links = document.querySelectorAll('.titles a');
 
-        /* find the title element */
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
 
-        /* get the title from the title element */
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+    /*[DONE]remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList)
+    /*  [DONE] for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    console.log(articles)
+    for (let article of articles) {
+        /*[DONE] get the article id */
+        const articleId = article.getAttribute('id');
+        console.log("id:" + articleId)
+        /* [DONE]find the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        console.log("articule title: " + articleTitle)
+
+        /* IN PROGRESSget the title from the title element */
 
         /* create HTML of the link */
 
         /* insert link into titleList */
-
     }
-
-    generateTitleLinks();
 }
+
+generateTitleLinks();
+
