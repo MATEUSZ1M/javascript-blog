8207574869885a2a73bab9f190e297919ed8daa0
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-02-25 21:23:23 +0000

    Add  make a new constant tag and extract tag from the href constant

--- js/script.js
+++ js/script.js
@@ -118,11 +118,12 @@ function tagClickHandler(event){
   /* [DONE] make new constant named "clickedElement" and give it the value of "this" */
   const clickedElement = this;
   console.log(clickedElement);
-  /* [IN PROGRESS] make a new constant "href" and read the attribute "href" of the clicked element */
+  /* [DONE] make a new constant "href" and read the attribute "href" of the clicked element */
   const href = clickedElement.getAttribute('href');
   console.log('href of clicked tag: ' + href);
-  /* make a new constant "tag" and extract tag from the "href" constant */
-
+  /* [IN PROGRESS]make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+  console.log('clicked tag: ' + tag);
   /* find all tag links with class active */
 
   /* START LOOP: for each active tag link */
